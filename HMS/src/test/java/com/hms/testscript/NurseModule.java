cb2ab219bada95488df1acae1f616417eaa1d51e
sande <sande@KodingTree>
2026-06-05 09:23:38 +0530

    My First Commit Create nurse

---
+++ HMS/src/test/java/com/hms/testscript/NurseModule.java
@@ -0,0 +1,13 @@
+package com.hms.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class NurseModule {
+
+	@Test
+	public void testCreateNurse() {
+		Reporter.log("create nurse",true); 
+	}
+	
+}
